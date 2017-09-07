33ae061d57bda9e65fc2dbef853afacd9d216d66
Anders Dalen <anders.dalen@gmail.com>
2017-09-07 07:22:50 +0200

    Adding setup file with flask dependency

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name = 'ws',
+    version = '1.0',
+    description = 'python practice',
+    author = 'Anders Dalen',
+    author_email = 'andersdalen@gmail.com',
+    url = 'http://python-python.193b.starter-ca-central-1.openshiftapps.com/',
+    install_requires = ['Flask'],
+)
